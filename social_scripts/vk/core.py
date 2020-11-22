2390239d5c456b3429b8652e2b0f8daebc8900f3
Vlad Blazhko <mr_plum@mail.ru>
2020-11-22 01:24:19 +0100

    Add break condition

--- social_scripts/vk/core.py
+++ social_scripts/vk/core.py
@@ -1,3 +1,4 @@
+from typing import Callable
 from time import sleep
 
 import vk
@@ -8,10 +9,31 @@ from . import constants
 
 def execute_vkscripts(
         vkapi: vk.API, vk_scripts, sleep_time=constants.SLEEP_TIME, v=5.95,
-        desc_tqdm=None):
+        desc_tqdm=None, break_condition: Callable[[list], bool] = None):
+    """
+
+    :param vkapi:
+    :param vk_scripts:
+    :param sleep_time:
+    :param v:
+    :param desc_tqdm:
+    :param break_condition: retrieves results while condition is true
+    :return:
+    """
     results = []
     for code in tqdm(vk_scripts, desc=desc_tqdm):
-        results.extend(vkapi.execute(code=code, v=v))
+        batch_results = vkapi.execute(code=code, v=v)
+
+        if break_condition:
+            cond = True
+            for res in batch_results:
+                cond = break_condition(res)
+                if cond:
+                    results.append(res)
+            if not cond:
+                break
+        else:
+            results.extend(results)
         sleep(sleep_time)
 
     return results
